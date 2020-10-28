a2bfedd868646e5c36875c7aeebeecac5c5d5d75
DebbySpaninks <dspaninks@gmail.com>
2020-10-28 14:13:09 +0100

    add script.js

---
+++ opdracht-basic_arrow_functions/script.js
@@ -0,0 +1,16 @@
+// Deel A
+const ikRockArrowFunctions = () => {
+    console.log('Joe, ik rock de arrow functions!');
+};
+
+// Deel B
+const fivePlusSeven = () => 5 + 7
+
+// Deel C
+const myFunction = (a, b) => a + b
+
+// Deel D 
+const addFive = a => a + 5
+
+// Deel E 
+const createObject = () => ({greeting: 'hoi'})
